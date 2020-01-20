90ffaf2a190c8cce2f0e11039b54a40da65eebfa
Ru <ru@beyondstays.com>
2020-01-19 17:39:31 -0800

    linear reg

--- analysis.py
+++ analysis.py
@@ -1,2 +1,25 @@
-def chooseLabels(df):
-    return []
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+# reading csv file into a pandas dataframe
+data = pd.read_csv("data/day.csv")
+data = data.drop(['instant', 'dteday', 'yr'], 1)
+predicts = ['cnt']
+
+# finding strength of correlation between IV and DVs
+for predict in predicts:
+    for col in data.columns:
+        x = np.array(data[col])
+        y = np.array(data[predict])
+
+        plt.scatter(x, y)
+        plt.title(col + " vs " + predict + " with r: " + str(round(np.corrcoef(x, y)[1][0], 3)))
+        plt.show()
+
+# testing for hypothesized multicollinearity in atemp vs temp
+x = np.array(data['atemp'])
+y = np.array(data['temp'])
+plt.scatter(x, y)
+plt.title('atemp vs temp with r: ' + str(round(np.corrcoef(x, y)[1][0], 3)))
+plt.show()
--- model.py
+++ model.py
@@ -1,69 +1,70 @@
 import pandas as pd
 import numpy as np
-import sklearn
-from sklearn import linear_model
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
 from datetime import datetime
-from pandas.tseries.holiday import USFederalHolidayCalendar
 
 
 class currInfo():
 
     def __init__(self):
         self.curr = datetime.today()
-        self.weekday = -1
-        self.holiday = -1
 
-    def currMonth(self):
-        return self.curr.month
+    def currSeason(self):
+        doy = self.curr.timetuple().tm_yday
+        spring = range(80, 172)
+        summer = range(172, 264)
+        fall = range(264, 355)
 
-    def currHoliday(self):
-        holidays = USFederalHolidayCalendar().holidays().to_pydatetime()
-        if self.curr in holidays:
-            self.holiday = 1
-            return 1
+        if doy in spring:
+            return 2
+        elif doy in summer:
+            return 3
+        elif doy in fall:
+            return 4
         else:
-            self.holiday = 0
-            return 0
-
-    def currWeekday(self):
-        day_of_week = self.curr.isoweekday()
-        if day_of_week == 6 or day_of_week == 7:
-            self.weekday = 0
-            return 0
-        else:
-            self.weekday = 1
             return 1
 
     def currWorkingDay(self):
-        return 1 if not self.weekday and not self.holiday else 0
+        return bool(len(pd.bdate_range(self.curr, self.curr)))
 
     def selectRows(self, df):
-        df = df.loc[df['mnth'] == self.currMonth()]
-        df = df.loc[df['holiday'] == self.currHoliday()]
-        # df = df.loc[df['weekday'] == self.currWeekday()]
+        df = df.loc[df['season'] == self.currSeason()]
         # df = df.loc[df['workingday'] == self.currWorkingDay()]
         return df
-
 
 # reading csv file into a pandas dataframe
 data = pd.read_csv("data/day.csv")
 
 # data trimming
-predict = 'cnt'
-data = data.drop(['instant', 'dteday', 'yr', 'weathersit', 'season', 'casual', 'registered', 'weekday', 'workingday'], 1)
+data = data[['temp', 'season', 'weathersit', 'cnt']]
 curr = currInfo()
-data = curr.selectRows(data)
+# data = curr.selectRows(data)
 
-x = np.array(data.drop([predict], 1))
-y = np.array(data[predict])
+x = np.array(data.drop(['cnt'], 1))
+y = np.array(data['cnt'])
 
-# training
-x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size=0.1)
-model = linear_model.LinearRegression()
+accuracy_lst = []
 
-# fitting
-model.fit(x_train, y_train)
-accuracy = model.score(x_test, y_test)
-print("accuracy: " + str(accuracy) + "\n")
+for _ in range(1000):
+    # training
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+    poly = PolynomialFeatures(degree=2)
+    model = LinearRegression()
+
+    x_poly_train = poly.fit_transform(x_train)
+    x_poly_test = poly.fit_transform(x_test)
+
+    model.fit(x_poly_train, y_train)
+    y_pred = model.predict(x_poly_test)
+
+    accuracy = model.score(x_poly_test, y_test)
+    accuracy_lst.append(accuracy)
+
+accuracy_lst.sort()
+print("accuracy: " + str(accuracy_lst[500]) + "\n")
 
 # next day prediction
